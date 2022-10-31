792aede4adedd060dc9e9c4de536bc4bc1f0c8a5
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2022-10-31 19:29:18 +0200

    student taulun crud operaatiot toimii ja salasanan kryptaus

--- backend/models/student_model.js
+++ backend/models/student_model.js
@@ -1,28 +1,33 @@
 const db = require('../database');
+const bcrypt = require('bcryptjs');
+
+const saltRounds = 10;
 
 const student = {
-  getById: function(id, callback) {
-    return db.query('select * from student where id_student=?', [id], callback);
-  },
-  getAll: function(callback) {
-    return db.query('select * from student', callback);
-  },
-  add: function(add_data, callback) {
-    return db.query(
-      'insert into student (id_student,fname,lname,password) values(?,?,?,?)',
-      [add_data.id_student,add_data.fname, add_data.lname, add_data.password],
-      callback
-    );
-  },
-  delete: function(id, callback) {
-    return db.query('delete from student where id_student=?', [id], callback);
-  },
-  update: function(id, update_data, callback) {
-    return db.query(
-      'update student set fname=?,lname=?, password=? where id_student=?',
-      [update_data.fname, update_data.lname, update_data.password, id],
-      callback
-    );
-  }
+    getById: function (id, callback) {
+        return db.query('select * from student where id_student=?', [id], callback);
+    },
+    getAll: function (callback) {
+        return db.query('select * from student', callback);
+    },
+    add: function (add_data, callback) {
+        bcrypt.hash(add_data.password, saltRounds, function (err, hashedPassword) {
+            return db.query(
+                'insert into student (id_student,fname,lname,password) values(?,?,?,?)',
+                [add_data.id_student, add_data.fname, add_data.lname, hashedPassword],
+                callback);
+        });
+    },
+    delete: function (id, callback) {
+        return db.query('delete from student where id_student=?', [id], callback);
+    },
+    update: function (id, update_data, callback) {
+        bcrypt.hash(update_data.password, saltRounds, function (err, hashedPassword) {
+            return db.query(
+                'update student set fname=?,lname=?, password=? where id_student=?',
+                [update_data.fname, update_data.lname, hashedPassword, id],
+                callback);
+        });
+    }
 };
 module.exports = student;
