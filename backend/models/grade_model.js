cd86fc1ec8f5d2c4b31d31d49318118ccf5857bd
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2022-11-01 11:04:33 +0200

    grade taulun crud operaatiot toimii

---
+++ backend/models/grade_model.js
@@ -0,0 +1,27 @@
+const db = require('../database');
+
+
+const grade = {
+    getById: function (id, callback) {
+        return db.query('select * from grade where id_grade=?', [id], callback);
+    },
+    getAll: function (callback) {
+        return db.query('select * from grade', callback);
+    },
+    add: function (add_data, callback) {
+        return db.query(
+            'insert into grade (grade_date,id_student,id_course,grade) values(?,?,?,?)',
+            [add_data.grade_date, add_data.id_student, add_data.id_course, add_data.grade],
+            callback);
+    },
+    delete: function (id, callback) {
+        return db.query('delete from grade where id_grade=?', [id], callback);
+    },
+    update: function (id, update_data, callback) {
+            return db.query(
+                'update grade set grade_date=?,id_student=?, id_course=?, grade=? where id_grade=?',
+                [update_data.grade_date, update_data.id_student, update_data.id_course, update_data.grade, id],
+                callback);
+    }
+};
+module.exports = grade;
